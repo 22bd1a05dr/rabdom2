029c987b709c85ff9fd812bc2c49df7c213abc78
Sathvik <22bd1a05dr@gmail.com>
2024-11-16 11:20:33 +0530

    maven

---
+++ src/main/java/blabla/javamavebproject/App.java
@@ -0,0 +1,13 @@
+package blabla.javamavebproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
